676a59a9601adbc53a4b9a2e4cd2e363439cd0ba
Abdelwadoud Boukerma <abdelwadoudboukerma295@gmail.com>
2022-09-11 09:24:57 +0100

    Add MyLearn

---
+++ MyLearn/script.js
@@ -0,0 +1,14 @@
+function add7(number){
+    console.log(number + 7)
+}
+add7(5)
+
+function Multiply(num1, num2){
+    console.log(num1 * num2)
+}
+Multiply(5, 7)
+
+function capitalize(word){
+    console.log(word.toUpperCase())
+}
+capitalize("Hi")
